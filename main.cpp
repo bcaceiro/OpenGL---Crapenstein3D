b20eb8c49ee3e03b5e1940182c2f12f4f95af583
D. Cardoso <d_cardoso@d.cardoso>
2014-05-23 10:13:45 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Crapenstein
+//
+//  Created by Bruno Caceiro on 20/05/14.
+//  Copyright (c) 2014 Bruno Caceiro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "OLÁAA\n";
+    return 0;
+}
