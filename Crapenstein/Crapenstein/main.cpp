b74b8ad9931c713895f914186233556705ae8e69
Bruno Caceiro <bcaceiro@gmail.com>
2014-05-23 10:29:02 +0100

    Merdas fodas

---
+++ Crapenstein/Crapenstein/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Crapenstein
+//
+//  Created by Bruno Caceiro on 23/05/14.
+//  Copyright (c) 2014 Bruno Caceiro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
